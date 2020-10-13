be29adb7ef649effd12bff44cf873703929848e8
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-10-13 11:19:25 +0300

    Postprocessing script to accept file path as a parameter

--- postprocess.py
+++ postprocess.py
@@ -1,32 +1,38 @@
+import sys
 import json
 from util import getCanonical
 
-with open('./problems/3labels.json') as json_file:
-  ctr = 0
-  data = json.load(json_file)
-  for idx, problem in enumerate(data):
-    constrs = problem["constraint"]
-    lowerBound = problem["lower-bound"]
+def postprocess():
+  filePath = sys.argv[1]
+  with open(filePath) as json_file:
+    ctr = 0
+    data = json.load(json_file)
+    for idx, problem in enumerate(data):
+      constrs = problem["constraint"]
+      lowerBound = problem["lower-bound"]
 
-    if lowerBound == "":
-      newConstr1 = set([getCanonical(x.replace("3", "1")) for x in constrs])
-      newConstr2 = set([getCanonical(x.replace("2", "1")) for x in constrs])
-      newConstr3 = set([getCanonical(x.replace("3", "2")) for x in constrs])
-      newConstrs = [newConstr1, newConstr2, newConstr3]
+      if lowerBound == "":
+        newConstr1 = set([getCanonical(x.replace("3", "1")) for x in constrs])
+        newConstr2 = set([getCanonical(x.replace("2", "1")) for x in constrs])
+        newConstr3 = set([getCanonical(x.replace("3", "2")) for x in constrs])
+        newConstrs = [newConstr1, newConstr2, newConstr3]
 
-      prevProblems = data[:idx]
-      for prevProblem in prevProblems:
-        prevConstrs = set(prevProblem["constraint"])
-        prevLowerBound = prevProblem["lower-bound"]
+        prevProblems = data[:idx]
+        for prevProblem in prevProblems:
+          prevConstrs = set(prevProblem["constraint"])
+          prevLowerBound = prevProblem["lower-bound"]
 
-        if prevLowerBound != "" and prevLowerBound != "Θ(1)" and prevConstrs in newConstrs:
-          data[idx]["lower-bound"] = prevLowerBound
-          print(constrs, prevLowerBound)
-          ctr += 1
-          break
+          if prevLowerBound != "" and prevLowerBound != "Θ(1)" and prevConstrs in newConstrs:
+            data[idx]["lower-bound"] = prevLowerBound
+            print(constrs, prevLowerBound)
+            ctr += 1
+            break
 
-  print("Total number of updated lower bounds: %s" % ctr)
+    print("Total number of updated lower bounds: %s" % ctr)
 
-with open('./problems/3labels.json', 'w', encoding='utf-8') as f:
-  json.dump(data, f, ensure_ascii=False, indent=2)
+  with open(filePath, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
 
+
+if __name__ == "__main__":
+   postprocess()
