da93849a3b41fe7f3355a7049cd06614af8fb7c1
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-01-28 14:07:57 +0200

    Add postprocess sub-module to a list of packages in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="brt-classifier",
-    version="0.1.0",
+    version="0.1.1",
     description="A command-line tool for automatically classifying binary and ternary labelling problems on bipartite rooted trees (hence BRT).",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["brt_classifier"],
+    packages=["brt_classifier", "brt_classifier.postprocess"],
     data_files=[
         ('data',['brt_classifier/problems/2labels.json']),
         ('data',['brt_classifier/problems/3labels.json']),
